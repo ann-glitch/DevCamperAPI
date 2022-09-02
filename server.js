6e6465d6bbd7a9ad610933fab970a65a58db53db
ann <dorisann.me@gmail.com>
2022-09-02 00:57:20 +0000

    connected our database

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-// const logger = require("./middleware/logger");
-
-//route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
+
+//mongodb connection
+connectDB();
+
+//route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -21,6 +24,16 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+// handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`MongoServerError: ${err.message}`);
+
+  //close server and exit process
+  server.close(() => {
+    process.exit(1);
+  });
 });
