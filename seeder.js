41f93522dc77842b125fc523323b50d5383fa68d
ann <dorisann.me@gmail.com>
2022-09-10 12:47:36 +0000

    added course model

--- seeder.js
+++ seeder.js
@@ -11,16 +11,22 @@ mongoose.connect(process.env.MONGO_URI);
 
 //load bootcamp model
 const Bootcamp = require("./models/Bootcamp");
+const Course = require("./models/Course");
 
-//read json files
+//read bootcamp json files
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+//read course json files
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
 );
 
 //import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log("Data Imported!".green.inverse);
     process.exit();
   } catch (err) {
@@ -32,6 +38,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log("Data Deleted!".red.inverse);
     process.exit();
   } catch (err) {
