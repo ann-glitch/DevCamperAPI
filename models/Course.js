e25ac675c292818d496606dcaad009b971379fa9
ann <dorisann.me@gmail.com>
2022-09-14 23:10:03 +0000

    added calculate average course cost functionality

--- models/Course.js
+++ models/Course.js
@@ -38,4 +38,38 @@ const CourseSchema = new mongoose.Schema({
   },
 });
 
+//calculate averageCost
+CourseSchema.statics.getAverageCost = async function (bootcampId) {
+  console.log("calculating average cost...".blue);
+
+  const obj = await this.aggregate([
+    {
+      $match: { bootcamp: bootcampId },
+    },
+    {
+      $group: {
+        _id: "$bootcamp",
+        averageCost: { $avg: "$tuition" },
+      },
+    },
+  ]);
+  try {
+    await this.model("Bootcamp").findByIdAndUpdate(bootcampId, {
+      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//call averageCost after save
+CourseSchema.post("save", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
+//call averageCost before remove
+CourseSchema.pre("remove", function () {
+  this.constructor.getAverageCost(this.bootcamp);
+});
+
 module.exports = mongoose.model("Course", CourseSchema);
