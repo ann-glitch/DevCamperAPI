9fe69cb78ba235999e95b2887ddf000d32bcacb8
ann <dorisann.me@gmail.com>
2022-09-16 00:34:05 +0000

    added advanced results middleware

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,76 +9,7 @@ const { isGeneratorFunction } = require("util/types");
 // @route  /api/v1/bootcamps
 // @access  public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query;
-
-  //copy query
-  let reqQuery = { ...req.query };
-
-  //create query string
-  let queryStr = JSON.stringify(reqQuery);
-
-  //fields to exclude
-  const removeFields = ["select", "sort", "page", "limit"];
-
-  //loop over removeFields and delete them from reqQuery
-  removeFields.forEach((param) => delete reqQuery[param]);
-
-  //create operators($gt, $gte, $lte, $lt, in)
-  queryStr = queryStr.replace(
-    /\b(gte|gt|lte|lt|in)\b/g,
-    (match) => `$${match}`
-  );
-
-  //finding resource
-  query = Bootcamp.find(JSON.parse(queryStr)).populate("courses");
-
-  //select fields
-  if (req.query.select) {
-    const fields = req.query.select.split(",").join(" ");
-    query = query.select(fields);
-  }
-
-  //sort fields
-  if (req.query.sort) {
-    const sortBy = req.query.sort.split(",").join(" ");
-    query = query.sort(sortBy);
-  } else {
-    query = query.sort("-createdAt");
-  }
-
-  //pagination
-  const page = parseInt(req.query.page, 10) || 1;
-  const limit = parseInt(req.query.limit, 10) || 25;
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-  const total = await Bootcamp.countDocuments();
-
-  query = query.skip(startIndex).limit(limit);
-
-  //executing query
-  const bootcamps = await query;
-
-  //pagination results
-  const pagination = {};
-
-  if (endIndex < total) {
-    pagination.next = {
-      page: page + 1,
-      limit,
-    };
-  } else if (startIndex > 0) {
-    pagination.prev = {
-      page: page - 1,
-      limit,
-    };
-  }
-
-  res.status(200).json({
-    success: true,
-    count: bootcamps.length,
-    pagination,
-    data: bootcamps,
-  });
+  res.status(200).json(res.advancedResults);
 });
 
 // @description  Get bootcamp
--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,6 +10,9 @@ const {
 } = require("../controllers/bootcamps");
 const { route } = require("./courses");
 
+const Bootcamp = require("../models/Bootcamp");
+const advancedResults = require("../middleware/advancedResults");
+
 //include other resources
 const courseRouter = require("./courses");
 
@@ -18,7 +21,10 @@ const router = express.Router();
 //re-route into other resource routers
 router.use("/:bootcampId/courses", courseRouter);
 
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(createBootcamp);
 
 router.route("/:id/photos").patch(bootcampPhotoUpload);
 
