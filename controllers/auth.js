ef772ff5601466b1bb7262d417491303cc4d8d35
ann <dorisann.me@gmail.com>
2022-09-18 20:56:46 +0000

    added user encrypting password and json  web token

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,5 +6,13 @@ const User = require("../models/User");
 // @route  /api/v1/auth/register
 // @access  public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  //create user
+  const user = await User.create({ name, email, password, role });
+
+  //create token
+  const token = user.getSignedWebToken();
+
+  res.status(200).json({ success: true, token });
 });
