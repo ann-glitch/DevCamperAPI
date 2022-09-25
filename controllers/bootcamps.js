0d9b61b80e5e7c89a130c4ff1041c4eba0c09232
ann <dorisann.me@gmail.com>
2022-09-25 18:11:51 +0000

    added bootcamp user relationship

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -34,6 +34,21 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route  /api/v1/bootcamps
 // @access  private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user
+  req.body.user = req.user.id;
+
+  //check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+
+  //a publisher can only publish one bootcamp unlike an admin.
+  if (publishedBootcamp && req.user.role !== "admin") {
+    return next(
+      new ErrorResponse(
+        `The user with id ${req.user.id} has already published a bootcamp.`
+      )
+    );
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
--- seeder.js
+++ seeder.js
@@ -12,14 +12,21 @@ mongoose.connect(process.env.MONGO_URI);
 //load bootcamp model
 const Bootcamp = require("./models/Bootcamp");
 const Course = require("./models/Course");
+const User = require("./models/User");
 
 //read bootcamp json files
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
 );
+
 //read course json files
 const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, "utf-8")
+);
+
+//read users json files
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, "utf-8")
 );
 
 //import into DB
@@ -27,7 +34,10 @@ const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
+
     console.log("Data Imported!".green.inverse);
+
     process.exit();
   } catch (err) {
     console.log(err);
@@ -39,7 +49,10 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
+
     console.log("Data Deleted!".red.inverse);
+
     process.exit();
   } catch (err) {
     console.log(err);
